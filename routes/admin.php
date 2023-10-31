986ce809fc96a838846aa583dbdd92b8483d04de
Mayorie43 <141832487+Mayorie43@users.noreply.github.com>
2023-10-31 21:11:50 +0100

    Add files via upload

---
+++ routes/admin.php
@@ -0,0 +1,15 @@
+<?php
+use App\Http\Controllers\Admin\AdminController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AuthController;
+
+
+Route::controller(AuthController::class)->prefix('admin/')->name('admin.')->group(function(){
+    Route::get('login', 'showloginform')->name('login');
+    
+
+});
+
+Route::controller(AdminController::class)->prefix('admin/')->name('admin.')->group(function(){
+    Route::get('dashboard', 'dashboard')->name('dashboard');
+});
